f56c1a60609034e06dab8dd83c040464ad8d165d
nikoaaman <nikolasaaman@gmail.com>
2023-04-11 19:57:41 -0300

    database and hero

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("hola mundo")
+const contenedor = document.getElementById("products")
+fetch("./database.json")
+    .then(response => response.json())
+    .then(data => {
+        for(const producto of data){
+            contenedor.innerHTML += `
+            <div class="producto">
+                <h3>${producto.name}</h3>
+            </div>
+            `
+        }
+    })

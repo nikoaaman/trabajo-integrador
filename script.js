db5a8fdd2bd3b47f5cd93c52c09a629920d4d8ca
nikoaaman <nikolasaaman@gmail.com>
2023-04-12 09:47:33 -0300

    estructura

--- script.js
+++ script.js
@@ -7,7 +7,17 @@ fetch("./database.json")
             contenedor.innerHTML += `
             <div class="producto">
                 <h3>${producto.name}</h3>
+                <img src="${producto.imagen}" alt="${producto.name}" class="foto-producto">
+                <p class="price">${"$"+producto.price}</p>
             </div>
             `
         }
     })
+const formulario = document.getElementById("form")
+formulario.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    console.log(formulario.nombre.value)
+    console.log(formulario.email.value)
+    console.log(formulario.mensaje.value)
+    formulario.reset()
+})
